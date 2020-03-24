5c1ba49abe3fd9498c211a370e49574cdcc5464b
WtotdeD <wouter.dijk.e@gmail.com>
2020-03-24 13:11:15 +0100

    My first sharp project

---
+++ BranachesAndLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BranachesAndLoops
+{
+    class Program
+    {
+        static void ExploreIF()
+        {
+            int a = 1;
+            int b = 4;
+            if ((a + b > 10) && (a * b <15) || (a + b == 50))
+                {
+                Console.WriteLine("The sum off a and b is greater then 10");
+                Console.WriteLine("and the sum is smaller then fifteen");
+                }
+            else
+            Console.WriteLine("The sum is not greater then ten");
+        }
+
+
+         static void Main(string[] args)
+         {
+             int number;
+             Console.WriteLine("Welcome.....");
+             System.Threading.Thread.Sleep(0500);
+             Console.WriteLine("To the most advanced calculator ever");
+             System.Threading.Thread.Sleep(0500);
+             Console.WriteLine("So mortal..... what is the number you choise...");
+            number  = Convert.ToInt32(Console.ReadLine());
+             
+             ExploreIF();
+         }
+
+    }
+}
---
+++ NumbersInCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 30 ;
+            int c = 14;
+            int d = (a + b ) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient : {e} ");
+            Console.WriteLine($"remainder : {d}");
+            
+        }
+    }
+}
